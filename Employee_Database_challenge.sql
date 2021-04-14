b5461ec8868ac8827dda43ad226a7bebed9a8dde
Maya Jefferson <mayajefferson@Mayas-MacBook-Pro.local>
2021-04-14 08:20:20 -0500

    Deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -28,3 +28,20 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+--Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+e.last_name,
+    e.birth_date,
+    ti.title,
+    de.from_date
+    de.to_date
+--INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
